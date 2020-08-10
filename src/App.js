0316e5175221fa02f37bd3f7e0c0ceecff377dc9
Atieh <64927398+atiehamidi@users.noreply.github.com>
2020-08-10 14:14:47 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+import { Switch, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Loading from "./components/Loading";
+import MessageBox from "./components/MessageBox";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
+import { useDispatch, useSelector } from "react-redux";
+import { selectAppLoading } from "./store/appState/selectors";
+import { getUserWithStoredToken } from "./store/user/actions";
+import { Jumbotron } from "react-bootstrap";
+
+const Home = () => (
+  <Jumbotron>
+    <h1>Home</h1>
+  </Jumbotron>
+);
+const Other = () => (
+  <Jumbotron>
+    <h1>Other</h1>
+  </Jumbotron>
+);
+
+function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectAppLoading);
+
+  useEffect(() => {
+    dispatch(getUserWithStoredToken());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <Navigation />
+      <MessageBox />
+      {isLoading ? <Loading /> : null}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/other" component={Other} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
