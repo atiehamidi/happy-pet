3fdccf2e9405b3b32541608804dee8ab50e1f9a3
Atieh Hamidi <atieh@MacBook-Pro.local>
2020-08-16 22:52:11 +0200

    added the form for adding a  new pet

--- src/pages/NewPet/index.js
+++ src/pages/NewPet/index.js
@@ -1,22 +1,113 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectToken } from "../../store/user/selectors";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Col } from "react-bootstrap";
 
 export default function NewPet() {
   const token = useSelector(selectToken);
   const history = useHistory();
   const { id } = useParams();
-
+  const [type, set_type] = useState("");
+  const [name, set_name] = useState("");
+  const [birth, set_birth] = useState();
+  const [sex, set_sex] = useState("other");
+  const [image, set_image] = useState("");
+  const [breed, set_breed] = useState("");
+  const [description, set_description] = useState("");
   useEffect(() => {
     if (token === null) {
       history.push("/");
     }
   }, [token, history]);
+
+  function submitForm(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="background">
       <div className="transbox">
-        <form></form>
+        <Container>
+          <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+            <h3>Please fill the form</h3>
+            <Form.Group controlId="formBasicType">
+              <Form.Label>type</Form.Label>
+              <Form.Control
+                value={type}
+                onChange={(event) => set_type(event.target.value)}
+                type="text"
+                placeholder="Enter Type"
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                value={name}
+                onChange={(event) => set_name(event.target.value)}
+                type="text"
+                placeholder="Enter Name"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicBirth">
+              <Form.Label>Birthday</Form.Label>
+              <Form.Control
+                value={birth}
+                onChange={(event) => set_birth(event.target.value)}
+                type="Date"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicSex">
+              <Form.Label>Sex</Form.Label>
+              <Form.Control
+                as="select"
+                value={sex}
+                onChange={(event) => set_sex(event.target.value)}
+              >
+                <option>female</option>
+                <option>male</option>
+                <option>other</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicImageUrl">
+              <Form.Label>Image url</Form.Label>
+              <Form.Control
+                value={image}
+                onChange={(event) => set_image(event.target.value)}
+                type="file"
+                placeholder="http://"
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicBreed">
+              <Form.Label>Breed</Form.Label>
+              <Form.Control
+                value={breed}
+                onChange={(event) => set_breed(event.target.value)}
+                type="text"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                value={description}
+                onChange={(event) => set_description(event.target.value)}
+                type="text"
+              />
+            </Form.Group>
+            <Form.Group className="mt-5">
+              <Button variant="primary" type="submit" onClick={submitForm}>
+                submit
+              </Button>
+            </Form.Group>
+          </Form>
+        </Container>
       </div>
     </div>
   );
