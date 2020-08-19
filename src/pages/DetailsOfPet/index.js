95d63cbe9dd558739ff1930c4a6fff84ea6c1889
Atieh Hamidi <atieh@MacBook-Pro.local>
2020-08-19 10:36:43 +0200

    dispalyed data of a pet

--- src/pages/DetailsOfPet/index.js
+++ src/pages/DetailsOfPet/index.js
@@ -16,12 +16,34 @@ export default function DetailsOfPet() {
   useEffect(() => {
     token === null ? history.push("/") : dispatch(fetchPet(id));
   }, [token, history]);
+
+  const birth = Date.parse(Pet.birthday);
+  const today = new Date();
+  today.getDate();
+  const currentDate = Date.parse(today);
+  var minutes = 1000 * 60;
+  var hours = minutes * 60;
+  var days = hours * 24;
+  var years = days * 365;
+
+  const age = Math.round((currentDate - birth) / years);
   return (
     <div>
       {" "}
       <div className="background">
         <div className="transbox">
           <p>name:{Pet.name}</p>
+          <p>type:{Pet.type}</p>
+          <p>age:{age} </p>
+          <p>breed:{Pet.breed}</p>
+          {Pet.orders.map((order) => {
+            return (
+              <div key={order.id}>
+                <p>{order.total}</p>
+                {order.done ? "?" : "no"}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
