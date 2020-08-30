a691c403f427e6aa853fadb41a61f17e47a05991
Atieh Hamidi <hamidiatiye@gmail.com>
2020-08-30 19:41:15 +0200

    added some styling

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../store/user/selectors";
+import "./index.css";
 
 export default function Dashboard() {
   const user = useSelector(selectUser);
@@ -10,15 +11,19 @@ export default function Dashboard() {
   return (
     <div className="background">
       <div className="transbox">
-        <h4>Profile</h4>
-        <p>firstname:{user.firstName}</p>
-        <p>Lastname:{user.lastName}</p>
-        <p>Email:{user.email}</p>
-        <p>Phone:{user.phone}</p>
-        <button className="button1">Edit</button>
-        <Link to={`/${user.id}/newpet`}>
-          <button className="button1">Add new pet</button>
-        </Link>
+        <div calssName="flex-container">
+          <h4>Profile</h4>
+          <p>firstname:{user.firstName}</p>
+          <p>Lastname:{user.lastName}</p>
+          <p>Email:{user.email}</p>
+          <p>Phone:{user.phone}</p>
+        </div>
+        <div>
+          <button className="button1">Edit</button>
+          <Link to={`/${user.id}/newpet`}>
+            <button className="button1">new pet</button>
+          </Link>
+        </div>
       </div>
       <div className="transbox">
         {user.pets.map((pet) => {
@@ -30,9 +35,9 @@ export default function Dashboard() {
                   style={{ width: "150px", height: "150px" }}
                 />
                 <p style={{ textAlign: "center" }}>{pet.name}</p>
-                <p style={{ textAlign: "center" }}>{pet.breed}</p>
+
                 <button className="button1">Edit</button>
-                <button className="button1">New Request</button>
+                <button className="button1">Request</button>
               </div>
             </div>
           );
