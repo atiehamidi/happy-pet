d58795a122b5d51bb4dea17fcb945249d9c0d91e
Atieh <hamidiatiye@gmail.com>
2020-10-12 16:18:16 +0200

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
-export const apiUrl = "https://happy-pet-1.herokuapp.com/";
+export const apiUrl = "https://happy-pet-1.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const apiKeyGoogle = process.env.REACT_APP_GOOGLE;
